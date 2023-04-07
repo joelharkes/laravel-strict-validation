35ef39027e2c605cd39e547007529fd6883313b4
joelharkes <jlharkes@gmail.com>
2023-04-07 21:07:21 +0200

    Add validIn tests

---
+++ tests/ValidInTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Joelharkes\LaravelStrictValidation\Tests;
+
+use Joelharkes\LaravelStrictValidation\Rules\ValidIn;
+
+class ValidInTest extends BaseTest
+{
+    public function testPassesForInteger(): void
+    {
+        $validator = $this->validaterFor(new ValidIn([10]), 10);
+        $this->assertTrue($validator->passes());
+        $this->assertSame(10, $validator->validated()['input']);
+    }
+
+    public function testIsCastToInteger(): void
+    {
+        $validator = $this->validaterFor(new ValidIn([10]), '10');
+        $this->assertTrue($validator->passes());
+        $this->assertSame(10, $validator->validated()['input']);
+    }
+
+    public function testPassesForString(): void
+    {
+        $validator = $this->validaterFor(new ValidIn(['10']), '10');
+        $this->assertTrue($validator->passes());
+        $this->assertSame('10', $validator->validated()['input']);
+    }
+
+    public function testIsCastToString(): void
+    {
+        $validator = $this->validaterFor(new ValidIn(['10']), 10);
+        $this->assertTrue($validator->passes());
+        $this->assertSame('10', $validator->validated()['input']);
+    }
+}
